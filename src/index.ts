2869765abf8f539f843efb5fab765ab830c03078
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-07-24 13:58:19 +0700

    chore: yes

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { common } from 'replugged';
+
+const { toast } = common;
+
+export const start = (): void => {};
+
+export const stop = async (): Promise<void> => {};
+
+export * from './settings';
